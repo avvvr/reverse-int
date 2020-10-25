19a971e12893817387e93e873ad858a9abeb7231
Alina Balysh <lina.balysh@gmail.com>
2020-10-25 14:25:34 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let strArr = String(n).split('');
+    if (strArr[0] === '-') {
+        strArr.shift();
+    }
+    return Number(strArr.reverse().join(''));
 }
